6cc294a0e6aec8c45eb504cb3c5890c641fa6966
Hossein <hossenmsignin@gmail.com>
2022-04-08 06:08:36 +0430

    rev3

--- script.py
+++ script.py
@@ -4,6 +4,78 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+def fft_u(y,t):
+    from scipy.fftpack import fft
+    # Number of sample points
+    N = t
+    # sample spacing
+    T = 1.0
+    x = np.arange(N)
+    yf = fft(y,)
+    xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+    import matplotlib.pyplot as plt
+    plt.rcParams['figure.figsize'] = (30, 10)
+    plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+    plt.grid()
+    plt.ylim(0,0.25)
+    plt.show()
+# fft_u(np.array(snn_model.layers[3].history_U[1:])[:,3],t)
+
+
+def plot_delta_comparison(snn_model,xx, t, l=None, ylim=None, xmlim=-0.1, xlim=None, nnn=False):
+    for lay in range(len(snn_model.layers) - 1):
+        print(lay)
+        if l is not None and l==lay: continue
+        for n in range(len(snn_model.layers[lay].U)):
+            plt.rcParams['figure.figsize'] = (30, 1.5)
+            # real_u=np.array(snn_model.layers[lay].history_U).T[n]
+            # abs_u=np.array([snn_model.abstract_v_layer(xx,tt,lay) for tt in range(t+1)]).T[n]
+
+            if nnn:
+                real_u = np.array(snn_model.layers[lay].history_N).T[n]
+                abs_u = np.array([snn_model.abstract_n_layer(xx, tt, lay) for tt in range(t + 1)]).T[n]
+
+            plt.step(np.arange(len(abs_u)), abs_u - real_u, where='post')
+
+            if ylim is not None: plt.ylim(-ylim, ylim)
+            if xlim is not None: plt.xlim(xmlim, xlim)
+
+            plt.grid()
+            plt.show()
+
+
+def plot_v_comparison(snn_model,xx, t, lay, ylim=None, xlim=None, xmlim=-0.1, ymlim=-0.1):
+    for n in range(len(snn_model.layers[lay].U)):
+        plt.rcParams['figure.figsize'] = (40, 5)
+        real_u=np.array(snn_model.layers[lay].history_U).T[n]
+        plt.step(np.arange(len(real_u)), real_u, where='post')
+
+        abs_u=np.array([snn_model.abstract_v_layer(xx,tt,lay) for tt in range(t+1)]).T[n]
+        plt.step(np.arange(len(abs_u)), abs_u, where='post')
+
+        if ylim is not None: plt.ylim(ymlim,ylim)
+        if xlim is not None: plt.xlim(xmlim,xlim)
+
+        plt.grid()
+        plt.show()
+
+
+def plot_n_comparison(snn_model,xx, t, lay, ylim=None, xlim=None, xmlim=-0.1, ymlim=-0.1):
+    for n in range(len(snn_model.layers[lay].U)):
+        plt.rcParams['figure.figsize'] = (40, 5)
+        real_n=np.array(snn_model.layers[lay].history_N).T[n]
+        plt.step(np.arange(len(real_n)), real_n, where='post')
+
+        abs_n=np.array([snn_model.abstract_n_layer(xx,tt,lay) for tt in range(t+1)]).T[n]
+        plt.step(np.arange(len(abs_n)), abs_n, where='post')
+
+        if ylim is not None: plt.ylim(ymlim,ylim)
+        if xlim is not None: plt.xlim(xmlim,xlim)
+
+        plt.grid()
+        plt.show()
+
+
 def make_dataset():
     x1 = np.random.normal(0, 1, (4, 2))
     # concatenate two 2d vectors
@@ -41,7 +113,11 @@ def make_model(x, y):
             super(Net, self).__init__()
             self.fc1 = nn.Linear(2, 4)
             self.fc2 = nn.Linear(4, 5)
-            self.fc3 = nn.Linear(5, 2)
+            self.fc3 = nn.Linear(5, 5)
+            self.fc4 = nn.Linear(5, 4)
+            self.fc5 = nn.Linear(4, 3)
+            self.fc6 = nn.Linear(3, 2)
+            self.fc7 = nn.Linear(2, 2)
 
         def forward(self, x):
             x = self.fc1(x)
@@ -49,12 +125,20 @@ def make_model(x, y):
             x = self.fc2(x)
             x = torch.relu(x)
             x = self.fc3(x)
+            x = torch.relu(x)
+            x = self.fc4(x)
+            x = torch.relu(x)
+            x = self.fc5(x)
+            x = torch.relu(x)
+            x = self.fc6(x)
+            x = torch.relu(x)
+            x = self.fc7(x)
             x = torch.sigmoid(x)
             return x
 
     model = Net()
     # train model on x and y
-    optimizer = torch.optim.Adamax(model.parameters(), lr=0.08)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.05)
     loss_func = nn.MSELoss()
     for epoch in range(2000):
         prediction = model(torch.from_numpy(x).float())
@@ -78,14 +162,15 @@ def get_changed_weight(x, wb, model):
         a.append(eval('model.fc%s(b[-1])' % (i + 1)))
         b.append(torch.relu(a[-1]))
     b.pop(-1)
-    b.append(torch.sigmoid(a[-1] - 5))
+    b.append(torch.sigmoid(a[-1]))
+
     for i in range(len(a)):
         a[i] = a[i].data.numpy().copy()
         b[i] = b[i].data.numpy().copy()
     a.pop(0)
     b.pop(0)
 
-    f = lambda x: abs(x).max()  # np.percentile(abs(x).max(),99.9)
+    f = lambda x: np.percentile(x.max(),99.9) # np.percentile(abs(x).max(),99.9)
     wb = wb.copy()
     for i in range(len(wb[0])):
         for j in range(len(wb[0][i])):
